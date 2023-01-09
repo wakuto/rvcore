b3b07fd4d9222c6117bcdfdd9a49a8c7dacefe83
wakuto <waku.902002@gmail.com>
2023-01-10 08:59:42 +0900

    超途中

--- rtl/fifo.sv
+++ rtl/fifo.sv
@@ -65,25 +65,25 @@ module async_fifo(
   output logic [31:0] dout,
   output logic        empty
 );
-  parameter FULL_THRS = 2;
+  parameter FULL_THRS = 60;
   parameter EMPTY_THRS = 2;
 
-  logic [5:0] w_addr, r_addr;
-  logic [5:0] w_addr_gray, r_addr_gray;
+  logic [6:0] w_addr, r_addr;
+  logic [6:0] w_addr_gray, r_addr_gray;
 
   simple_dual_port_ram ram(
     .wr_clk(w_clk),
-    .wr_addr(w_addr),
+    .wr_addr(w_addr[5:0]),
     .wr_din(din),
     .wr_en(wen),
     .rd_clk(r_clk),
-    .rd_addr(r_addr),
+    .rd_addr(r_addr[5:0]),
     .rd_dout(dout),
     .rd_en(ren)
   );
 
   bin_gray_counter #(
-    .ADDR_WIDTH(6)
+    .ADDR_WIDTH(7)
   ) r_bgc (
     .clk(r_clk),
     .reset(0),
@@ -93,7 +93,7 @@ module async_fifo(
   );
 
   bin_gray_counter #(
-    .ADDR_WIDTH(6)
+    .ADDR_WIDTH(7)
   ) w_bgc (
     .clk(w_clk),
     .reset(0),
@@ -102,18 +102,18 @@ module async_fifo(
     .gray(w_addr_gray)
   );
 
-  function [5:0] gray2bin(
-    input logic [5:0] gray
+  function [6:0] gray2bin(
+    input logic [6:0] gray
   );
     int i;
-    gray2bin[5] = gray[5];
-    for (i = 5; i > 0; i = i - 1) begin
+    gray2bin[6] = gray[6];
+    for (i = 6; i > 0; i = i - 1) begin
       gray2bin[i-1] = gray2bin[i] ^ gray[i-1];
     end
   endfunction
 
-  logic [5:0] w_gray_buff1, w_gray_buff2;
-  logic [5:0] r_gray_buff1, r_gray_buff2;
+  logic [6:0] w_gray_buff1, w_gray_buff2;
+  logic [6:0] r_gray_buff1, r_gray_buff2;
 
   // write -> read への受け渡し
   always_ff @(posedge r_clk) begin
@@ -127,17 +127,27 @@ module async_fifo(
     r_gray_buff2 <= r_gray_buff1;
   end
 
+  logic w_same_msb, r_same_msb;
   logic [6:0] w_addr_diff, r_addr_diff;
-  logic [5:0] w_gray2bin, r_gray2bin;
+  logic [6:0] w_gray2bin, r_gray2bin;
+
   assign w_gray2bin = gray2bin(r_gray_buff2);
   assign r_gray2bin = gray2bin(w_gray_buff2);
-  assign w_addr_diff = w_addr - w_gray2bin;
-  assign r_addr_diff = r_gray2bin - r_addr;
+
+  assign w_same_msb = w_addr[6] ~^ w_gray2bin[6];
+  assign r_same_msb = r_gray2bin[6] ~^ r_addr[6];
+  assign w_addr_diff = {~w_same_msb, w_addr[5:0]} - 6'(w_gray2bin[5:0]);
+  assign r_addr_diff = {~r_same_msb, r_gray2bin[5:0]} - 6'(r_addr[5:0]);
   // assign w_addr_diff = w_addr - gray2bin(r_gray_buff2);
   // assign r_addr_diff = gray2bin(w_gray_buff2) - r_addr;
+  // if w_addr[6] == w_gray2bin[6] {
+  //   w_addr_diff = w_addr[5:0] - w_gray2bin[5:0];
+  // } else {
+  //   w_addr_diff = {{1'b1, w_addr[5:0]} - {1'b0, w_gray2bin[5:0]}}[5:0];
+  // }
 
-  assign full  = w_addr_diff[6] ? 0 : w_addr_diff[5:0] > FULL_THRS;
-  assign empty = r_addr_diff[6] ? 0 : r_addr_diff[5:0] < EMPTY_THRS;
+  assign full  = w_addr_diff[6] ? 1 : w_addr_diff[5:0] > FULL_THRS;
+  assign empty = r_addr_diff[6] ? 1 : r_addr_diff[5:0] < EMPTY_THRS;
 
 endmodule
 `default_nettype wire
--- rtl/i_cache.sv
+++ rtl/i_cache.sv
@@ -25,6 +25,12 @@ module i_cache (
 
   logic cache_miss;
 
+  // 4 word = 16Byte = 1Line = 4bit
+  // 512Byte = 32Line = 5bit
+  logic        valid [31:0];
+  logic [22:0] tag [31:0];        // 32bit - 5bit(line addr) - 4bit(line offset)
+  logic [32*4-1:0]  cache [31:0]; // 4word * 32Line(=5bit addr)
+
   // cpu側転送
   always_ff @(posedge clk) begin
     if (reset) begin
@@ -60,6 +66,95 @@ module i_cache (
       axi_rready <= next_rready;
     end
   end
+  // instruction fetch!!!
+  // valid = 0;
+  // do_cache_search()
+  // if (is_cache_hit) {
+  //   valid = 1;
+  //   return instruction;
+  // } else {
+  //   data_fetch_from_memory();
+  //   memory_write_to_fifo();
+  //   cache_append_from_fifo();
+  //   valid = 1;
+  //   return instruction;
+  // }
 
+endmodule
+
+module bram #(
+  parameter DATA_WIDTH = 32,
+  parameter CAPACITY = 1024
+)(
+  input  logic                  clk,
+  input  logic [ADDR_WIDTH-1:0] addr,
+  input  logic                  wen,
+  input  logic [DATA_WIDTH-1:0] din,
+  output logic [DATA_WIDTH-1:0] dout
+);
+  parameter ADDR_WIDTH = $clog2(CAPACITY/DATA_WIDTH);
+
+  (* ram_style = "block" *)
+  logic [DATA_WIDTH-1:0] ram [(1 << ADDR_WIDTH)-1:0];
+
+  always_ff @(posedge clk) begin
+    if (wen) ram[addr] <= din;
+    dout <= ram[addr];
+  end
+  
+endmodule
+
+module direct_map #(
+  parameter LINE_SIZE = 16,
+  parameter CACHE_SIZE = 1024
+)(
+  input  logic clk,
+  input  logic req_addr,
+  output logic valid,
+  output logic hit,
+  output logic [31:0] data
+);
+  parameter LINE_NUM = CACHE_SIZE/LINE_SIZE;
+  parameter LINE_OFFSET_WIDTH = $clog2(LINE_SIZE);
+  parameter SET_ADDR_WIDTH = $clog2(LINE_NUM);
+  parameter TAG_WIDTH = 32 - SET_ADDR_WIDTH - LINE_OFFSET_WIDTH;
+
+  logic [LINE_OFFSET_WIDTH-1:0] line_offset;
+  logic [SET_ADDR_WIDTH-1:0]    set_addr;
+  logic [TAG_WIDTH-1:0]    tag;
+
+  assign line_offset = req_addr[0 +: LINE_OFFSET_WIDTH];
+  assign set_addr    = req_addr[LINE_OFFSET_WIDTH +: SET_ADDR_WIDTH];
+  assign tag         = req_addr[LINE_OFFSET_WIDTH + SET_ADDR_WIDTH +: TAG_WIDTH];
+
+  logic [(LINE_SIZE << 3)-1:0] bram_out;
+
+  bram #(
+    .DATA_WIDTH(LINE_SIZE << 3),
+    .CAPACITY(CACHE_SIZE)
+  ) data (
+    .clk,
+    .addr(set_addr),
+    .wen(1'b0),
+    .din(0),
+    .dout(bram_out)
+  );
+
+  bram #(
+    .DATA_WIDTH(TAG_WIDTH+1),
+    .CAPACITY(LINE_NUM*
+  ) valid_and_tag (
+    .clk,
+    .addr(set_addr),
+    .wen(1'b0),
+    .din(0),
+    .dout(bram_out)
+  );
+
+  always_ff @(posedge clk) begin
+  end
+
+
+  
 endmodule
 `default_nettype wire
