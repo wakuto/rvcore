63a0c29698adb4cd720223426bcdd1253ed5b1e1
wakuto <waku.902002@gmail.com>
2023-10-18 19:35:09 +0900

    ちょっとだけエラーを修正

--- rtl/core/freelist.sv
+++ rtl/core/freelist.sv
@@ -11,25 +11,30 @@ module freelist(
   genvar i;
   generate
     for(i = 0; i < DISPATCH_WIDTH; i++) begin : genfifo
-      fifoIf.fifo fifo_if;
+      fifoIf #(.DATA_WIDTH(PHYS_REGS_ADDR_WIDTH), .DEPTH(DISPATCH_WIDTH)) fifo_if;
       fifo fifo(
         .clk,
         .rst,
-        .fifo_if(genfifo[i].fifo_if)
+        .fifo_if(fifo_if.fifo)
       );
     end
   endgenerate
-  always_comb begin
+  // TODO: FreeListってdispatchのレーン別に作っちゃうと、
+  // 各レーン間でフリーなレジスタの共有ができないからリソースに無駄が出そう
+  // （レーン0だけfreeListが空で、他のレーンは開いてるレジスタがたくさんあったりする状況が出そう）
+  generate
     for (i = 0; i < DISPATCH_WIDTH; i++) begin
-      genfifo[i].fifo_if.wr_en = freelist_if.push_en[i];
-      genfifo[i].fifo_if.wr_data = freelist_if.push_reg[i];
-      freelist_if.full[i] = genfifo[i].fifo_if.full;
+      always_comb begin
+        genfifo[i].fifo_if.wr_en = freelist_if.push_en[i];
+        genfifo[i].fifo_if.wr_data = freelist_if.push_reg[i];
+        freelist_if.full[i] = genfifo[i].fifo_if.full;
 
-      genfifo[i].fifo_if.rd_en = freelist_if.pop_en[i];
-      freelist_if.pop_reg[i] = genfifo[i].fifo_if.rd_data;
-      freelist_if.empty[i] = genfifo[i].fifo_if.empty;
+        genfifo[i].fifo_if.rd_en = freelist_if.pop_en[i];
+        freelist_if.pop_reg[i] = genfifo[i].fifo_if.rd_data;
+        freelist_if.empty[i] = genfifo[i].fifo_if.empty;
+      end
     end
-  end
+  endgenerate
 endmodule
 
 
